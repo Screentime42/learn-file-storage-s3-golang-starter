80ac6f0e97237a6b45b917088341e410cc3d7911
Screentime42 <danohare99@googlemail.com>
2026-05-07 12:52:16 +0100

    handler_upload_thumbnail.go lesson additions

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,59 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	// Fetch video meta data
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+
+	// Ensure authed user
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You do not own this video", err)
+		return
+	}
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	// Parse form data 
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	ct := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(ct)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid content type", err)
+		return
+	}
+
+	// Read all image data into a byte slice
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read file", err)
+		return
+	}
+
+	thumb := thumbnail{
+		data:			imageData,
+		mediaType: 	mediaType,
+	}
+
+	videoThumbnails[videoID] = thumb
+
+	// Update video metadata
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID.String())
+	video.ThumbnailURL = &thumbnailURL
+	updatedVideo := cfg.db.UpdateVideo(video)
+	
+
+
+	respondWithJSON(w, http.StatusOK, updatedVideo)
 }
